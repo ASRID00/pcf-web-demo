ccdaf85d3ef7729485746ba993fb494cdbd04279
Brad Jolicoeur <brad@jolicoeurs.net>
2018-07-23 16:01:05 -0400

    Add project files.

---
+++ pcf-web-demo/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using pcf_web_demo.Models;
+
+namespace pcf_web_demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IConfiguration Configuration { get; }
+
+        public HomeController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.ConfigLabel = Configuration["option1"];
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
